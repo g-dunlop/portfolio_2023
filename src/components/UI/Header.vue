7d90804d614fbdb4226690e9efd3baf98094c16f
g-dunlop <graemedunlop1983@gmail.com>
2022-12-29 12:06:20 +0000

    added nav links to header

--- src/components/UI/Header.vue
+++ src/components/UI/Header.vue
@@ -1,11 +1,15 @@
 <template>
     <div>
         Header.vue
+        <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
     </div>
 </template>
 
 <script setup>
-
+import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <style lang="scss" scoped>
