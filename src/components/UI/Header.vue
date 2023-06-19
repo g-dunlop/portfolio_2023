b0f756498fc30304e60d8d22ae5c26a9b1589b8c
g-dunlop <graemedunlop1983@gmail.com>
2023-06-19 09:48:24 +0100

    fix header responsiveness

--- src/components/UI/Header.vue
+++ src/components/UI/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="navbar  justify-between items-center">
         <div class="flex items-center">
-            <RouterLink class="mr-4" to="/"><img src="../../assets/favicon.ico" @click="setActive('home')"></RouterLink>
+            <RouterLink class="mr-4" to="/"><img src="/logo/favicon-32x32.png" @click="setActive('home')"></RouterLink>
             <h2 class=" transition-all ml-4 font-bold font-mono text-lg tracking-widest">graemedunlop.com</h2>
         </div>
         <nav class="w-7/10 hidden md:flex lg:flex justify-end">
@@ -15,22 +15,22 @@
                 <!-- <label tabindex="0" class="btn btn-ghost btn-circle">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                 </label> -->
-                <label class="btn btn-circle swap swap-rotate">
+                <label class="btn btn-circle btn-sm swap swap-rotate">
   
                 <!--this hidden checkbox controls the state -->
                 <input type="checkbox" :checked="checked" @click="setChecked" />
             
                 <!-- hamburger icon -->
-                <svg class="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z"/></svg>
+                <svg class="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z"/></svg>
             
                 <!-- close icon -->
-                <svg class="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"/></svg>
+                <svg class="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"/></svg>
   
                 </label>
                 
             </div>
         </nav>
-        <RouterLink to="/login" class="btn">Login</RouterLink>
+        <RouterLink to="/login" class="btn btn-sm lg:flex md:flex hidden">Login</RouterLink>
     </div>
     <ul tabindex="0" v-if="checked" class="absolute right-0 top-13 mt-3 p-2 shadow bg-base-100 w-10/10 text-center">
         <RouterLink to="/" class="w-10/10 hover:bg-neutral hover:text-white" ><li class="w-10/10 hover:bg-neutral hover:text-white cursor-pointer" @click="setActive('home')">HomePage</li></RouterLink>
--- src/components/projects/ProjectsList.vue
+++ src/components/projects/ProjectsList.vue
@@ -1,55 +1,13 @@
 <template>
     <!-- make card component -->
     <div>
-            <!-- <div class="lg:flex flex-wrap w-100/100 justify-center  py-8 bg-slate-100">
-                <div v-for="project in projects" class=" card card-compact lg:w-1/3 md:w90/100 sm:w-90/100 mb-6  mx-6 bg-white shadow-2xl">
-                    <figure class="w-10/10 h-15rem"><img class="w-10/10 h-15rem" :src="project.thumbnail_url" alt="car!"/></figure>
-                    <div class="card-body bg-white rounded-xl h-15rem">
-                        <div class="flex justify-between">
-                            <h2 class="card-title">{{project.name}}</h2>
-                            <hr />
-                            <span class="badge badge-lg text-xs bg-red-500">{{project.date}}</span>
-                        </div>
-                        <p class="font-extralight text-s h-50px">{{ project.description_short }}</p>
-                        <ul class="h-50px">
-                            <li class="text-xs tracking-widest badge bg-black m-1" v-for="tool in project.tools">{{ tool }}</li>
-                        </ul>
-                        <div class="card-actions justify-end">
-                            <button class="btn btn-primary btn-sm" @click="navigate(project.id)">ReadMe!</button>
-                        </div>
-                    </div>
-                </div>
-            </div> -->
-
-            <!-- use tiny slider for this? -->
-            <div class="h-screen pt-8 px-8">
-                <h2 class="text-center font-bold text-3xl mb-4">Projects</h2>
-                <div class="w-full flex justify-center" v-if="!projects">
-                    <Loading />
-                </div>
-            <div class="carousel w-10/10 " v-if="projects" >
+        <div class="h-screen pt-8 px-8">
+            <h2 class="text-center font-bold text-3xl mb-4">Projects</h2>
+            <div class="w-full flex justify-center" v-if="projects.length < 1">
+                <Loading />
+            </div>
+            <div class="carousel w-10/10 " v-if="projects.length > 1" >
                 <ProjectsListItem :projects="projects" />
-                <!-- <div :id="index+1" class="carousel-item w-full bg-gray-100 rounded-lg" v-for="project, index in projects">
-                    <div class="w-full flex flex-col lg:flex-row p-2 items-center">
-                    <figure class="w-10/10 h-full rounded cursor-pointer" @click="navigate(project.id)">
-                        <iframe class="w-full h-80"  :src="project.video_url" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;" allowfullscreen></iframe>
-                    </figure>
-                    <div class="w-10/10 lg:w-8/10 p-8 h-full bg-white rounded ">
-                        <div class="flex justify-between">
-                            <h2 class="card-title">{{project.name}}</h2>
-                            <span class="badge badge-lg border-0 text-sm bg-gray-500">{{project.date}}</span>
-                        </div>
-                        <p class="font-extralight text-s py-4">{{ project.description_short }}</p>
-                        <ul class="py-4">
-                            <li class="text-xs tracking-widest badge bg-blue-800 m-1" v-for="tool in project.tools"><p class="">{{ tool }}</p></li>
-                        </ul>
-                        <div class="card-actions justify-end">
-                            <button class="btn btn-sm" @click="navigate(project.id)">ReadMe!</button>
-                        </div>
-                    </div>
-                </div>
-                </div> -->
-                
             </div> 
             <div class="flex justify-center w-10/10 py-4 gap-2">
                 <a :href="`#${index+1}`" class="btn btn-xs hover:btn-accent active:" :class="[isActive[index+1] ? 'btn-accent': 'btn-neutral']" v-for="project, index in projects" @click="setActive(index+1)">{{index+1}}</a> 
